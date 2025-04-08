99a2afe5fdacb0530a94ef43d5da9819c173aff4
ihorzhuk <ihorzhuk94@gmail.com>
2025-04-08 22:07:12 +0300

    new changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl text-violet-700 font-black">Welcome to the World of Next.js</h1>
+    <>
+      <h1 className="h1-bold font-black">
+        Welcome to the World of Next.js
+      </h1>
+      <h2 className="h1-bold font-space-grotesk">
+        Welcome to the World of Next.js
+      </h2>
+    </>
   );
 }
 
