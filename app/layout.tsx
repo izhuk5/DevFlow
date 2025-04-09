d6d101bda275af4ff3ab92cc351664196e49c1f9
ihorzhuk <ihorzhuk94@gmail.com>
2025-04-09 14:55:47 +0300

    adding metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,8 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A better version of StackOverflow",
+  description: "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: "/public/images/site-logo.svg"
 };
 
 export default function RootLayout({
